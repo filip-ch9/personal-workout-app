5505a360d7da48088d0bd5653b0c369c062cf912
Filip Churlevski <filip.churlevski@gmail.com>
2021-02-13 10:30:45 +0100

    [initial commit]

--- src/main/java/com/codeacademyfinalproject/personalworkoutapp/repository/WorkoutProgramRepository.java
+++ src/main/java/com/codeacademyfinalproject/personalworkoutapp/repository/WorkoutProgramRepository.java
@@ -1,13 +1,16 @@
 package com.codeacademyfinalproject.personalworkoutapp.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.codeacademyfinalproject.personalworkoutapp.model.Group;
 import com.codeacademyfinalproject.personalworkoutapp.model.WorkoutProgram;
 
 @Repository
-public interface WorkoutProgramRepository extends JpaRepository<WorkoutProgram, Long>{
+public interface WorkoutProgramRepository extends JpaRepository<WorkoutProgram, Long> {
 	
-	
+	List<WorkoutProgram> findByGroup(Group group);
 	
 }
--- src/main/java/com/codeacademyfinalproject/personalworkoutapp/view/controller/CoachProfileController.java
+++ src/main/java/com/codeacademyfinalproject/personalworkoutapp/view/controller/CoachProfileController.java
@@ -2,20 +2,21 @@ package com.codeacademyfinalproject.personalworkoutapp.view.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.codeacademyfinalproject.personalworkoutapp.service.CoachService;
 
 @Controller
-@SessionAttributes(names = {"username", "users"})
+@SessionAttributes(names = {"username", "users", "workoutPrograms"})
 public class CoachProfileController {
 	
 	@Autowired
 	private CoachService coachService;
 	
 	@GetMapping("/coach-profile")
-	public String showProfilePage() {
+	public String showProfilePage(ModelMap model) { 
 		return "coach-profile";
 	}
 	
